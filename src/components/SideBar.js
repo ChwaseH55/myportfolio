c444ad98e58a9b884001dddb389f8fde5695d423
ChwaseH55 <chasehanson27@gmail.com>
2024-11-07 16:50:35 -0500

    add leetcode link, fix images on projects page

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { FaGithub, FaLinkedin, FaEnvelope } from "react-icons/fa"; // Import icons
 import { FaXTwitter } from "react-icons/fa6";
+import { SiLeetcode } from "react-icons/si";
 
 const Sidebar = () => {
   return (
-    <aside className="bg-black w-16 h-1/3 fixed top-1/2 left-2 transform -translate-y-1/2 flex flex-col items-center p-4 space-y-5 z-20 rounded shadow-hgold">
+    <aside className="bg-black w-16 h-80 fixed top-1/2 left-2 transform -translate-y-1/2 flex flex-col items-center p-4 space-y-5 z-20 rounded shadow-hgold">
       <div className="flex flex-col items-center justify-center space-y-8 h-full">
         <a
           href="https://github.com/ChwaseH55"
@@ -12,7 +13,15 @@ const Sidebar = () => {
           rel="noopener noreferrer"
           className="text-gold hover:text-hgold flex items-center justify-center"
         >
-          <FaGithub className="text-3xl" /> {/* Adjust the size here */}
+          <FaGithub className="text-3xl" />
+        </a>
+        <a
+          href="https://leetcode.com/u/ChaseH77/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-gold hover:text-hgold flex items-center justify-center"
+        >
+          <SiLeetcode className="text-3xl" />
         </a>
         <a
           href="https://linkedin.com/in/chase-hanson-78b960330"
@@ -20,7 +29,7 @@ const Sidebar = () => {
           rel="noopener noreferrer"
           className="text-gold hover:text-hgold flex items-center justify-center"
         >
-          <FaLinkedin className="text-3xl" /> {/* Adjust the size here */}
+          <FaLinkedin className="text-3xl" />
         </a>
         <a
           href="https://X.com/ChaseHDev"
@@ -28,13 +37,13 @@ const Sidebar = () => {
           rel="noopener noreferrer"
           className="text-gold hover:text-hgold flex items-center justify-center"
         >
-          <FaXTwitter className="text-3xl" /> {/* Adjust the size here */}
+          <FaXTwitter className="text-3xl" />
         </a>
         <a
           href="mailto:chase.hanson9@gmail.com"
           className="text-gold hover:text-hgold flex items-center justify-center"
         >
-          <FaEnvelope className="text-3xl" /> {/* Adjust the size here */}
+          <FaEnvelope className="text-3xl" />
         </a>
       </div>
     </aside>
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -2,7 +2,7 @@
 import React from "react";
 import useGradDirection from "../components/GradDirection";
 import Sidebar from "../components/SideBar"; // Reusing the Sidebar component
-import '../styles/Home.css'; // Ensure your CSS file is included
+import '../styles/Home.css'; // Ensure CSS file is included
 
 const ProjectsPage = () => {
   const gradientAngle = useGradDirection();
@@ -13,25 +13,25 @@ const ProjectsPage = () => {
       title: "UCF Library Group Finder",
       description: "A project to create a website and mobile app that serves the purpose of mapping out each floor of the UCF library allowing the creation of study groups, and an expansive forum feature",
       skills: "MongoDB, Express, Node.js, React, JavaScript, TailwindCSS, Flutter, Heroku",
-      thumbnail: "../AKS.png", // Path to the image
+      thumbnail: "/AKS.png", // Path to the image
     },
     {
       title: "UCF Golf Club Website and Mobile App (In Progress)",
       description: "This is my current Senior Design project. In a group of 6 student developers, I am the project manager attempting to deliver a product that can solve the current problem the Golf Club at UCF faces with a lack of a centralized club platform",
       skills: "Node.js, Express, PostgreSQL, React, JavaScript, TailwindCSS, Flutter, AWS",
-      thumbnail: "../Landing.png", // Path to the image
+      thumbnail: "/Landing.png", // Path to the image
     },
     {
       title: "M.E.O.W (In Progress)",
       description: "A rhythm based bullet-hell game. My personal contributions are mostly on the backend of the game, involving programming a fully functional rhythm indicator and developing the main mechanic of the game, allowing actions to only be performed on beat with the music",
       skills: "C#, Unity Game Editor",
-      thumbnail: "../Demo.png",
+      thumbnail: "/Demo.png",
     },
     {
       title: "Personal Portfolio",
       description: "I believe this project best demonstrates the frontend skills I have obtained working on several web apps. It is a personal showcase of my ability to create professional and creative designs, knowledge of React and Tailwind libraries, and an effective use of JavaScript",
       skills: "React, TailwindCSS, JavaScript",
-      thumbnail: "../portfolioThumbnail.png", // Path to the image
+      thumbnail: "/portfolioThumbnail.png", // Path to the image
     },
     // Add more projects as needed
   ];
